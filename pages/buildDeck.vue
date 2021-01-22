4a6c918c3f7b36ab52cafe637c524f28b43734e2
Kohei Terashita <kou0704terasita@gmail.com>
2021-01-22 21:00:46 +0900

    UPDATE: 検索画面をドロワーに変更

--- pages/buildDeck.vue
+++ pages/buildDeck.vue
@@ -1,115 +1,129 @@
 <template>
   <v-container>
-    <v-row>
-      <v-col cols="12" sm="3">
-        <v-tabs v-model="tab" grow>
-          <v-tab>ALL</v-tab>
-          <v-tab><v-icon>mdi-bookmark-multiple</v-icon></v-tab>
-        </v-tabs>
-        <div class="white">
-          <v-radio-group v-model="selectedRadio" mandatory row>
-            <div class="d-flex flex-wrap flex-row">
-              <v-flex
-                v-for="radioItem in radioItems"
-                :key="radioItem.color"
-                md3
-              >
-                <v-radio
-                  class="v-radio-wrapper"
-                  :name="radioItem.name"
-                  :label="radioItem.name"
-                  :color="radioItem.color"
-                ></v-radio>
-              </v-flex>
-            </div>
-          </v-radio-group>
-        </div>
-        <v-form>
-          <v-text-field
-            class="mt-2"
-            solo
-            append-icon="mdi-magnify"
-            placeholder="ユニット名で検索"
-          ></v-text-field>
-        </v-form>
-        <v-tabs-items v-model="tab">
-          <v-tab-item>
-            <v-list height="400" class="overflow-y-auto">
-              <v-list-item
-                v-for="card in cardList"
-                :key="card._id"
-                @click="cards.push(card)"
-              >
-                <v-list-item-avatar>
-                  <v-img :src="card.avatar" />
-                </v-list-item-avatar>
-                <v-list-item-content>
-                  <v-list-item-subtitle
-                    v-text="card._id"
-                  ></v-list-item-subtitle>
-                  <v-list-item-subtitle
-                    v-text="card.title"
-                  ></v-list-item-subtitle>
-                  <v-list-item-title v-text="card.unitName"></v-list-item-title>
-                </v-list-item-content>
-                <v-list-item-action>
-                  <v-btn>
-                    <v-icon>mdi-star</v-icon>
-                  </v-btn>
-                </v-list-item-action>
-              </v-list-item>
-              <infinite-loading
-                ref="infiniteLoading"
-                spinner="spiral"
-                @infinite="infiniteHandler"
-              >
-                <span slot="no-more">No More Cards</span>
-                <span slot="no-results">No Data</span>
-              </infinite-loading>
-            </v-list>
-          </v-tab-item>
-          <v-tab-item>
-            <v-card>お気に入りカード</v-card>
-          </v-tab-item>
-        </v-tabs-items>
-      </v-col>
-      <v-col cols="12" sm="9">
-        <div class="d-flex justify-space-between">
-          <v-form
-            v-model="deckName"
-            class="w-20 d-flex align-center"
-            @submit.prevent
-          >
-            <v-text-field type="text" label="DeckName"></v-text-field>
-          </v-form>
-          <div class="d-flex align-center">
-            <div>
-              <v-btn class="primary" width="100" @click="shareDeck"
-                >ツイート</v-btn
-              >
-              <v-btn class="info" width="100" @click="saveDeck">保存</v-btn>
-            </div>
+    <v-navigation-drawer app clipped width="400" v-model="searchDrawer">
+      <v-tabs v-model="tab" grow>
+        <v-tab>ALL</v-tab>
+        <v-tab><v-icon>mdi-bookmark-multiple</v-icon></v-tab>
+      </v-tabs>
+      <v-container>
+        <v-radio-group v-model="selectedRadio" mandatory row>
+          <div class="d-flex flex-wrap flex-row">
+            <v-flex v-for="radioItem in radioItems" :key="radioItem.color" md3>
+              <v-radio
+                class="v-radio-wrapper"
+                :name="radioItem.name"
+                :label="radioItem.name"
+                :color="radioItem.color"
+              ></v-radio>
+            </v-flex>
           </div>
+        </v-radio-group>
+      </v-container>
+      <v-form>
+        <v-text-field
+          class="mt-2"
+          solo
+          append-icon="mdi-magnify"
+          placeholder="ユニット名で検索"
+        ></v-text-field>
+      </v-form>
+      <v-tabs-items v-model="tab">
+        <v-tab-item>
+          <v-list height="400" class="overflow-y-auto">
+            <v-list-item
+              v-for="card in cardList"
+              :key="card._id"
+              @click="cards.push(card)"
+            >
+              <v-list-item-avatar>
+                <v-img :src="card.avatar" />
+              </v-list-item-avatar>
+              <v-list-item-content>
+                <v-list-item-subtitle v-text="card._id"></v-list-item-subtitle>
+                <v-list-item-subtitle
+                  v-text="card.title"
+                ></v-list-item-subtitle>
+                <v-list-item-title v-text="card.unitName"></v-list-item-title>
+              </v-list-item-content>
+              <v-list-item-action>
+                <v-btn>
+                  <v-icon @click.prevent="markCards.push(card)"
+                    >mdi-star</v-icon
+                  >
+                </v-btn>
+              </v-list-item-action>
+            </v-list-item>
+            <infinite-loading
+              ref="infiniteLoading"
+              spinner="spiral"
+              @infinite="infiniteHandler"
+            >
+              <span slot="no-more">No More Cards</span>
+              <span slot="no-results">No Data</span>
+            </infinite-loading>
+          </v-list>
+        </v-tab-item>
+        <v-tab-item>
+          <v-list height="400" class="overflow-y-auto">
+            <v-list-item
+              v-for="card in markCards"
+              :key="card._id"
+              @click="cards.push(card)"
+            >
+              <v-list-item-avatar>
+                <v-img :src="card.avatar" />
+              </v-list-item-avatar>
+              <v-list-item-content>
+                <v-list-item-subtitle v-text="card._id"></v-list-item-subtitle>
+                <v-list-item-subtitle
+                  v-text="card.title"
+                ></v-list-item-subtitle>
+                <v-list-item-title v-text="card.unitName"></v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+            <!-- <infinite-loading
+              ref="infiniteLoading"
+              spinner="spiral"
+              @infinite="infiniteHandler"
+            >
+              <span slot="no-more">No More Cards</span>
+              <span slot="no-results">No Data</span>
+            </infinite-loading> -->
+          </v-list>
+        </v-tab-item>
+      </v-tabs-items>
+    </v-navigation-drawer>
+
+    <div class="d-flex justify-space-between">
+      <v-form
+        v-model="deckName"
+        class="w-20 d-flex align-center"
+        @submit.prevent
+      >
+        <v-text-field type="text" label="DeckName"></v-text-field>
+      </v-form>
+      <div class="d-flex align-center">
+        <div>
+          <v-btn class="primary" width="100" @click="shareDeck">ツイート</v-btn>
+          <v-btn class="info" width="100" @click="saveDeck">保存</v-btn>
         </div>
-        <v-card>
-          <draggable
-            v-model="cards"
-            class="d-flex flex-wrap"
-            group="cards"
-            :animation="200"
-            @start="drag = true"
-            @end="drag = false"
-            @change="onMoveCard"
-          >
-            <PickedCard
-              v-for="(card, i) in cards"
-              :key="i"
-              :img="card"
-            ></PickedCard>
-          </draggable>
-        </v-card>
-      </v-col>
-    </v-row>
+      </div>
+    </div>
+    <draggable
+      v-model="cards"
+      class="d-flex flex-wrap"
+      group="cards"
+      :animation="200"
+      @start="drag = true"
+      @end="drag = false"
+      @change="onMoveCard"
+    >
+      <PickedCard
+        v-for="(card, i) in cards"
+        :key="i"
+        :img="card.image"
+      ></PickedCard>
+    </draggable>
   </v-container>
 </template>
 
@@ -125,6 +139,7 @@ export default {
     return {
       tab: null,
       deckName: '',
+      searchDrawer: null,
       nextPage: null,
       lastCard: null,
       selectedRadio: 0,
@@ -170,28 +185,8 @@ export default {
           color: 'brown',
         },
       ],
-      cards: [
-        require('@/static/img/S10/S10-001ST.png'),
-        require('@/static/img/S10/S10-001ST.png'),
-        require('@/static/img/S10/S10-001ST.png'),
-        require('@/static/img/S10/S10-001ST.png'),
-        require('@/static/img/S10/S10-002ST.png'),
-        require('@/static/img/S10/S10-002ST.png'),
-        require('@/static/img/S10/S10-002ST.png'),
-        require('@/static/img/S10/S10-002ST.png'),
-        require('@/static/img/S10/S10-003ST.png'),
-        require('@/static/img/S10/S10-003ST.png'),
-        require('@/static/img/S10/S10-003ST.png'),
-        require('@/static/img/S10/S10-003ST.png'),
-        require('@/static/img/S10/S10-004ST.png'),
-        require('@/static/img/S10/S10-004ST.png'),
-        require('@/static/img/S10/S10-004ST.png'),
-        require('@/static/img/S10/S10-004ST.png'),
-        require('@/static/img/S10/S10-005ST.png'),
-        require('@/static/img/S10/S10-005ST.png'),
-        require('@/static/img/S10/S10-005ST.png'),
-        require('@/static/img/S10/S10-005ST.png'),
-      ],
+      cards: [],
+      markCards: [],
       cardList: [],
     }
   },
@@ -283,13 +278,14 @@ export default {
       this.lastCard = this.nextPage.docs[this.nextPage.size - 1]
     },
     displayCards() {
-      const cardList = this.nextPage.docs.map((doc) => {
+      const cardData = this.nextPage.docs.map((doc) => {
         return doc.data()
       })
-      cardList.forEach((card) => {
+      cardData.forEach((card) => {
+        card.image = require('~/static/img/B01/B01-001SR.png')
         this.cardList.push(card)
       })
-      return cardList
+      return cardData
     },
     infiniteHandler($state) {
       setTimeout(async () => {
