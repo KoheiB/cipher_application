4bb941741f8dece07805b4497bb3de3cd1e80ca2
Kohei Terashita <kohei@KoheinoMacBook-Pro.local>
2021-02-13 00:52:58 +0900

    FIX: カードリストのコンポーネント、少し小さくした

--- components/PickedCardList.vue
+++ components/PickedCardList.vue
@@ -2,25 +2,25 @@
   <v-list-item
     class="px-0 mr-8 ml-2"
     :class="color"
-    style="margin-bottom: 1px"
+    style="margin-bottom: 1px; min-height: 38px"
     @click="$emit('card-list-click')"
   >
-    <v-list-item-avatar class="ma-0 mr-2" tile height="48" width="72">
+    <v-list-item-avatar class="ma-0 mr-2" tile height="38" width="72">
       <v-img :src="imageUrl" position="top" />
     </v-list-item-avatar>
     <v-list-item-content class="card-info py-0 d-flex flex-nowrap">
       <v-row class="mx-0">
         <v-col cols="10" class="py-0" align-self="center">
           <v-list-item-title>
-            <span class="card-title">{{ title }}</span>
+            <span class="list-title">{{ title }}</span>
             <br />
-            <span>{{ unitName }}</span>
+            <span class="list-unit-name">{{ unitName }}</span>
           </v-list-item-title>
         </v-col>
         <v-col
           cols="2"
-          class="px-0 py-4 blue-grey darken-4 white--text font-weight-bold text-center"
-          style="height: 48px"
+          class="px-0 blue-grey darken-4 white--text font-weight-bold text-center"
+          style="height: 38px; padding-top: 11px"
         >
           <span style="font-size: 16px; margin-right: 2px">x</span>{{ count }}
         </v-col>
@@ -65,8 +65,11 @@ export default {
 </script>
 
 <style>
-.card-title {
+.list-title {
   color: rgba(0, 0, 0, 0.6);
+  font-size: 0.75rem;
+}
+.list-unit-name {
   font-size: 0.875rem;
 }
 </style>
