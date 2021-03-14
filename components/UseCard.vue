21b984fc81ecf3306d580d1e811b07462774433d
Kohei Terashita <kohei@KoheinoMacBook-Pro.local>
2021-03-14 14:45:57 +0900

    #44-UPDATE: カードビュー

--- components/UseCard.vue
+++ components/UseCard.vue
@@ -1,26 +1,28 @@
 <template>
-  <div style="width: 10%">
+  <div style="width: 10%; padding: 1px">
     <v-hover>
       <template v-slot:default="{ hover }">
-        <v-card style="margin: 1px" hover>
+        <v-card>
           <v-img :src="imageUrl" />
           <v-fade-transition>
             <v-overlay v-show="hover" absolute>
-              <div class="d-flex flex-column">
+              <v-layout column>
+                <v-btn color="blue-grey lighten-4" x-small
+                  ><v-icon color="black">mdi-magnify</v-icon></v-btn
+                >
                 <v-btn
                   class="align-self-end"
                   color="blue-grey lighten-5"
-                  fab
                   x-small
                   ><v-icon color="light-blue accent-4">mdi-plus</v-icon></v-btn
                 >
-                <v-btn class="ma-1" color="blue-grey lighten-4" width="50%"
-                  ><v-icon color="black">mdi-magnify</v-icon></v-btn
-                >
-                <v-btn color="blue-grey lighten-5" fab x-small
+                <v-btn
+                  class="align-self-end"
+                  color="blue-grey lighten-5"
+                  x-small
                   ><v-icon color="red accent-4">mdi-minus</v-icon></v-btn
                 >
-              </div>
+              </v-layout>
             </v-overlay>
           </v-fade-transition>
         </v-card>
